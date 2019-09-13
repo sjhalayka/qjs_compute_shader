0e186d48bdb1990fe76a557c8c5f96f2aef75c3a
Shawn Halayka <sjhalayka@gmail.com>
2019-09-13 09:40:18 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
 	// Set up grid parameters
-	const size_t res = 100; // res must be 2 or greater
+	const size_t res = 1024; // res must be 2 or greater
 	const float grid_max = 1.5;
 	const float grid_min = -grid_max;
 	const float step_size = (grid_max - grid_min) / (res - 1);
@@ -33,9 +33,10 @@ int main(int argc, char **argv)
 	const size_t num_input_channels = 4;
 	vector<float> input_pixels(res * res * num_input_channels, 0.0f);
 
-	// Set up slice data, for use as input into the Marching Cubes algorithm
-	vector<float> slices(output_pixels.size() * res, 0.0f);
-	size_t slices_index = 0;
+	bool first_slice = true;
+	vector<float> last_slice = output_pixels;
+
+	vector<triangle> triangles;
 
 	// For each z slice
 	for (size_t z = 0; z < res; z++, Z.z += step_size)
@@ -68,29 +69,32 @@ int main(int argc, char **argv)
 			input_pixels,
 			max_iterations,
 			threshold,
-			C); 
+			C);
 
-		// Store output data for later use
-		for (size_t i = 0; i < output_pixels.size(); i++)
+		if (true == first_slice)
 		{
-			slices[slices_index] = output_pixels[i];
-			slices_index++;
+			// Do nothing... we need 2 slices for the
+			// tesselate_adjacent_xy_plane_pair function,
+			// and by this point we only have 1 slice.
+			first_slice = false;
+		}
+		else
+		{
+			// Now there are two slices, so gather the
+			// triangles for this slice pair
+			tesselate_adjacent_xy_plane_pair(
+				last_slice, output_pixels,
+				z - 1,
+				triangles,
+				threshold, // Use threshold as isovalue.
+				grid_min, grid_max, res,
+				grid_min, grid_max, res,
+				grid_min, grid_max, res);
 		}
 
-		// Note: One can also use the marching_cubes::tesselate_adjacent_xy_plane_pair
-		// function here to perform the tesselation of two adjacent vector<float> slices,
-		// rather than storing it all for later use. This way you only need to keep two 
-		// slices in memory, instead of all of the slices. This would make for lots of 
-		// room for triangles in memory. Of course, the syntax would become more complex, 
-		// and harder to follow.
-		//
-		// See https://github.com/sjhalayka/marching_cubes/blob/master/main.cpp for
-		// an example of the use of marching_cubes::tesselate_adjacent_xy_plane_pair
+		last_slice = output_pixels;
 	}
 
-	// Convert slice data to triangle mesh, save to STL file
-	vector<triangle> triangles; 
-	tesselate_field(slices, triangles, threshold, grid_min, grid_max, res);
 	write_triangles_to_binary_stereo_lithography_file(triangles, "out.stl");
 
 	return 0;
